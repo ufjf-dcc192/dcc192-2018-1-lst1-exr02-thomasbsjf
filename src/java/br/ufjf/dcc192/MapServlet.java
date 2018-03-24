49e7878e9bf3c0c104df7f44321821719e63c061
Thomas Santos <thomasbsjf@gmail.com>
2018-03-23 23:09:26 -0300

    Exercicio 2 Lista

---
+++ src/java/br/ufjf/dcc192/MapServlet.java
@@ -0,0 +1,83 @@
+package br.ufjf.dcc192;
+
+import java.util.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "MapServlet", urlPatterns = {"/map.html"})
+public class MapServlet extends HttpServlet {
+    
+    private Map<String, String> paises;
+    
+    public MapServlet(){
+        paises.put("Brasil", "Verde");
+        paises.put("Canadá", "Vermelho");
+        paises.put("Uruguai", "Azul");
+        paises.put("Japão", "Branco");
+        paises.put("Itália", "Roxo");
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Mapa Países</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Mapa Países</h1>");
+            out.println("<dl>");
+            
+            out.println("</dl>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
